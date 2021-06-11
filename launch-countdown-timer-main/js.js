58db1831929574aa0487fd40b8fd9d8ba845ff8a
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2021-06-11 19:24:48 +0500

    Update js.js

--- launch-countdown-timer-main/js.js
+++ launch-countdown-timer-main/js.js
@@ -1,4 +1,4 @@
-const finalDate = new Date("May 04, 2021 14:00:00").getTime();
+const finalDate = new Date("December 04, 2021 14:00:00").getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = finalDate - now;
